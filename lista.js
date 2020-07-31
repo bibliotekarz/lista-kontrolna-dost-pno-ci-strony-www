878614a39a8e65f82c5f739b9f39d1ba4f47e9be
bibliotekarz <arek@bibliotekarz.com>
2020-07-31 17:08:11 +0200

    pobieranie wyników do JSON

--- lista.js
+++ lista.js
@@ -1,42 +1,30 @@
 function statusOdpowiedzi(pytanie, wynik) {
     var x = document.getElementById(pytanie);
     sessionStorage.setItem(pytanie, wynik);
-    console.log(sessionStorage.getItem(pytanie) + " pobrał dane z " + pytanie);
-    if (wynik == 1) {
-        x.classList.add("ok");
-        x.classList.remove("no");
-        x.classList.remove("irrelevant");
-    } else if (wynik == 0) {
-        x.classList.remove("ok");
-        x.classList.add("no");
-        x.classList.remove("irrelevant");
-      }else {
-        x.classList.remove("no");
-        x.classList.remove("ok");
-        x.classList.add("irrelevant");
+
+    switch (wynik){
+        case "1":
+            x.classList.add("ok");
+            x.classList.remove("no");
+            x.classList.remove("irrelevant");
+            break;
+        case "0":
+            x.classList.remove("ok");
+            x.classList.add("no");
+            x.classList.remove("irrelevant");
+            break;
+        case "2":
+            x.classList.remove("no");
+            x.classList.remove("ok");
+            x.classList.add("irrelevant");
+            break;
+        default:
+            alert("kombinujemy ;)")    
     }
-    
 } 
 
 function pobierzOdpowiedzi(){
 
-    alert( JSON.stringify(localStorage));
+    alert( JSON.stringify(sessionStorage));
         
-        
-    console.log("po pętli");
-/*
-    let licznik = 0;
-    let lokata ="";
-    console.log(sessionStorage.length);
-    while (licznik <= sessionStorage.length) {
-        lokata = "py"+licznik;
-        console.log("lokalizacja: " + lokata)
-        let z = sessionStorage.key(lokata);
-        console.log("wartość z : " + z);
-        let animal = sessionStorage.getItem(z);
-        console.log("pokaz animal: " + animal);
-        
-        licznik++;
     }
-    */
-}
