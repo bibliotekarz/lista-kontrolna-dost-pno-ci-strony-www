10b312ed3cb69426132ea6c49f1911f101f2a435
bibliotekarz <arek@bibliotekarz.com>
2020-07-31 16:31:53 +0200

    wzbogacone css i js

--- lista.js
+++ lista.js
@@ -5,9 +5,38 @@ function statusOdpowiedzi(pytanie, wynik) {
     if (wynik == 1) {
         x.classList.add("ok");
         x.classList.remove("no");
-    } else {
+        x.classList.remove("irrelevant");
+    } else if (wynik == 0) {
         x.classList.remove("ok");
         x.classList.add("no");
+        x.classList.remove("irrelevant");
+      }else {
+        x.classList.remove("no");
+        x.classList.remove("ok");
+        x.classList.add("irrelevant");
     }
     
 } 
+
+function pobierzOdpowiedzi(){
+
+    alert( JSON.stringify(localStorage));
+        
+        
+    console.log("po pętli");
+/*
+    let licznik = 0;
+    let lokata ="";
+    console.log(sessionStorage.length);
+    while (licznik <= sessionStorage.length) {
+        lokata = "py"+licznik;
+        console.log("lokalizacja: " + lokata)
+        let z = sessionStorage.key(lokata);
+        console.log("wartość z : " + z);
+        let animal = sessionStorage.getItem(z);
+        console.log("pokaz animal: " + animal);
+        
+        licznik++;
+    }
+    */
+}
