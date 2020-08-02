3f55850c25123664f2fce1a44e37172b3fd73a50
bibliotekarz <arek@bibliotekarz.com>
2020-08-02 11:02:45 +0200

    dodana obsługa pytań kluczowych

--- lista.js
+++ lista.js
@@ -2,29 +2,31 @@ function statusOdpowiedzi(pytanie, wynik) {
     var x = document.getElementById(pytanie);
     sessionStorage.setItem(pytanie, wynik);
 
-    switch (wynik){
+    switch (wynik) {
         case "1":
             x.classList.add("ok");
-            x.classList.remove("no");
-            x.classList.remove("irrelevant");
+            x.classList.remove("no", "irrelevant","end", "end::before", "end::after");
             break;
         case "0":
-            x.classList.remove("ok");
+            x.classList.remove("ok", "irrelevant","end", "end::before", "end::after");
             x.classList.add("no");
-            x.classList.remove("irrelevant");
             break;
         case "2":
             x.classList.remove("no");
-            x.classList.remove("ok");
+            x.classList.remove("no", "ok", "end", "end::before", "end::after");
             x.classList.add("irrelevant");
             break;
+        case "3":
+            x.classList.remove("no", "ok", "irrelevant");
+            x.classList.add("end", "end::before", "end::after");
+            break;
         default:
-            alert("kombinujemy ;)")    
+            alert("kombinujemy ;)")
     }
-} 
+}
 
-function pobierzOdpowiedzi(){
+function pobierzOdpowiedzi() {
 
-    alert( JSON.stringify(sessionStorage));
-        
-    }
+    alert(JSON.stringify(sessionStorage));
+
+}
