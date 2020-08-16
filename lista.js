bd590dca6562678fcd9607d93833dfd71c5784c4
bibliotekarz <arek@bibliotekarz.com>
2020-08-16 16:08:31 +0200

    pierwsze poprawki UX

--- lista.js
+++ lista.js
@@ -70,10 +70,10 @@ function pobierzOdpowiedzi() {
     }
 
     function compareNumbers(a, b) {
-        return parseInt(a)>parseInt(b);
-     }
+        return parseInt(a) > parseInt(b);
+    }
 
-    let linia1 = "<table id=\"wynik\"><tr><th>Lp.</th><th>Pytanie</th><th>Stan</th><th>Odkryj pytanie</th></tr>";
+    let linia1 = "<table id=\"wynik\"><thead><tr><th class=\"nrpytania\">Lp.</th><th class=\"pytanieTablica\">Pytanie</th><th class=\"stan\">Stan</th><th class=\"odkryj\">Odkryj pytanie</th></thead></tr>";
     tablica.sort(compareNumbers);
 
     for (let i = 0; i < tablica.length; i++) {
@@ -83,13 +83,15 @@ function pobierzOdpowiedzi() {
         const kolumnaOpis = tablica[i][3];
         const kolumnadivPytania = tablica[i][4];
 
-        linia1 = linia1 + "<tr><td>" + kolumnaNrPytania + ".</td><td>" + kolumnaPytanieGole + "</td><td class=\"" + kolumnaKlasa + "\">" + kolumnaOpis + "</td><td><a href=\"#" + kolumnadivPytania + "\" onClick=\"odkryjPytanie(" + kolumnadivPytania + ", " + kolumnaNrPytania + ");\")>idź do pytania</a></td></tr>";
-        console.log(linia1);
+        linia1 = linia1 + "<tr><td class=\"nrpytania\">" + kolumnaNrPytania + ".</td><td class=\"pytanieTablica\">" + kolumnaPytanieGole + "</td><td class=\"" + kolumnaKlasa + " stan\">" + kolumnaOpis + "</td><td class=\"odkryj\"><a href=\"#" + kolumnadivPytania + "\" onClick=\"odkryjPytanie(" + kolumnadivPytania + ", " + kolumnaNrPytania + ");\")>idź do pytania " + kolumnaNrPytania + "</a></td></tr>";
+
     }
 
     linia1 = linia1 + "</table>";
 
     document.getElementById('miejsce').innerHTML += linia1;
+
+    window.location.assign("#miejsce");
 
 }
 
@@ -102,21 +104,22 @@ function odkryjPytanie(nrDiv, nr) {
     pobierzOdpowiedzi();
 }
 
-function kasujDane(){
+function kasujDane() {
     if (confirm("Czy chcesz wyczyścić zapisane dane?")) {
         localStorage.clear()
     }
-    
+
     let zaznaczone = document.querySelectorAll("INPUT[type='radio']");
-    for (let zaznaczony of zaznaczone){
+    for (let zaznaczony of zaznaczone) {
         zaznaczony.checked = false;
     }
 
-     let zaznaczoneDivy = document.querySelectorAll("p.pytanie");
-     for (let zaznaczonyDiv of zaznaczoneDivy){
+    let zaznaczoneDivy = document.querySelectorAll("p.pytanie");
+    for (let zaznaczonyDiv of zaznaczoneDivy) {
         zaznaczonyDiv.classList.remove("end", "no", "ok", "irrelevant");
         location.reload();
-     }
+        window.location.assign("#start");
+    }
 
 }
 
